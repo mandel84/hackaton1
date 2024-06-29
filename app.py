55c469b5a99ee02cf37c3e4792dc0e4a42270b22
mandel84 <mandel84@github.com>
2024-06-29 15:41:34 +0300

    first-commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import tweepy
+import psycopg2
+from psycopg2 import sql
+
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAADo%2BugEAAAAAyC9lM5yAv9wL8B4XG6wgY7cC2%2Bc%3DwTESCAlTVqRPN57gvaMeOQZnIXMmgS140arLMix9co5r2a1LBs'
+
+DB_NAME = 'antisemitism_db'
+DB_USER = 'postgres'
+DB_PASSWORD = 2384
+DB_HOST = 'localhost'
+DB_PORT = 5432
+
